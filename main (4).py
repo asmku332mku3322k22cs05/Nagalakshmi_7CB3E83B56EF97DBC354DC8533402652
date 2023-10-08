ba34048a61f2785e7d52aa28e332085480de9383
asmku332mku3322k22cs05 <144208750+asmku332mku3322k22cs05@users.noreply.github.com>
2023-10-08 19:15:26 +0530

    Add files via upload

---
+++ main (4).py
@@ -0,0 +1,24 @@
+"" "" ""
+Write a function called linear_search_product that takes the list of products and a target product 
+name as input. The function should perform a linear search to find the target product in the list and 
+return a list of indices of all occurrences of the product if
+found, or an empty list if the product is not found.
+"" "" ""
+
+
+def linearsearchproduct(productlist,targetprocduct):
+  induces=[]
+
+for index,priduct in enumerate(productlist):
+  if product ==targetproduct:
+    indices.append(index)
+
+  return indices
+
+
+#example indices
+product=["shoes","boot","loafes","sandal","shoes"]
+target="shoes"
+target ="apple"
+result=linearsearchproduct(product,target)
+print(result)
